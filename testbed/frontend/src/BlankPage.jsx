e4148791509dd0d5f354f47c87d99f7cd1664445
WilliamTakeshi <williamtpereira@gmail.com>
2025-10-09 10:12:37 +0200

    feat: first version of the frontend

---
+++ testbed/frontend/src/BlankPage.jsx
@@ -0,0 +1,8 @@
+export default function BlankPage() {
+  return (
+    <div className="text-gray-700 text-lg animate-fadeIn">
+      <h2 className="text-2xl font-semibold mb-4">Blank</h2>
+      <p>Blank page as a to do.</p>
+    </div>
+  );
+}
